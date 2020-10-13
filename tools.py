04babb884094820a2f0a209b875440d9adc30126
Arseniy Astankov <53515110+etidart@users.noreply.github.com>
2020-10-13 10:54:28 +0300

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,31 @@
+import json
+import codecs
+from os import system
+
+file_n = codecs.open("js/input.json", "r", "utf_8_sig")
+file_n_data = json.load(file_n)
+
+file_m = codecs.open("js/massa.json", "r", "utf_8_sig")
+file_m_data = json.load(file_m)
+
+print('Количество объектов: ' + str(file_m_data["massa"]))
+
+
+def chck():
+    het = input("\nСледующие действия: ")
+    if het == "LOOK":
+        print(file_n_data)
+        chck()
+    elif het == "CREATE":
+        crte = system("python crte.py")
+        chck()
+    elif het == "DELETE":
+        dlte = system("python dlte.py")
+        chck()
+    elif het == "QUIT":
+        quit()
+    else:
+        chck()
+
+
+chck()
